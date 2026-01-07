b6b8404ddf76dff5c8ed895b44eb894f21580bf2
ronniehilton01-art <ronniehilton01@gmail.com>
2026-01-07 00:27:57 -0800

    Refactor server.js to include wallet routes

--- server.js
+++ server.js
@@ -4,26 +4,27 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.js";
+import walletRoutes from "./routes/wallet.js";
 import paymentRoutes from "./routes/payment.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("Samaflux API running");
-});
-
-app.use("/api/auth", authRoutes);
+app.use("/auth", authRoutes);
+app.use("/api/wallet", walletRoutes);
 app.use("/api/payment", paymentRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT || 5000, () =>
-      console.log("Server started")
+    app.listen(process.env.PORT || 3000, () =>
+      console.log("Server running")
     );
   })
   .catch(err => console.error(err));
