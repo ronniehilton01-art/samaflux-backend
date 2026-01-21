219377c1ae3948650893c7f36c6ebd5e50b555a8
ronniehilton01-art <ronniehilton01@gmail.com>
2026-01-21 13:32:25 +0100

    Fix merge conflict in User model

--- models/User.js
+++ models/User.js
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  name: String,
-  balance: { type: Number, default: 0 }
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true },
+    password: String,
+    name: String,
+    balance: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("User", UserSchema);
-=======
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  name: String,
-  balance: { type: Number, default: 0 }
-}, { timestamps: true });
-
-export default mongoose.model("User", UserSchema);
->>>>>>> f65dadf (backend setup + jsonwebtoken installed)
