1ebc09304df35f0779a12f8c40541de37581eeca
ronniehilton01-art <ronniehilton01@gmail.com>
2026-01-24 03:01:02 -0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,12 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema(
-  {
-    email: { type: String, unique: true },
-    password: String,
-    name: String,
-    balance: { type: Number, default: 0 },
-  },
-  { timestamps: true }
-);
-
-export default mongoose.model("User", UserSchema);
+const UserSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  name: String,
+  phone: String,
+  address: String,
+  city: String,
+  state: String,
+  country: String,
+  zip: String,
+  balance: { type: Number, default: 0 }
+}, { timestamps: true });
